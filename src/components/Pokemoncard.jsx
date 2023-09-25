b0cac80756c7c22eef09e9098fd2ab77e32b2141
FlavinouTheOne <flavien.dumas63@gmail.com>
2023-09-25 18:31:15 +0200

    branch components.1 added

---
+++ src/components/Pokemoncard.jsx
@@ -0,0 +1,11 @@
+
+
+const PokemonCard = () => {
+    return (
+        <figure>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemonCard" />
+            <figcaption>Bulbasaur</figcaption>
+        </figure>
+    )
+}
+export default PokemonCard;
