503abf41a2e3e9d4671924a3df9c2cdc5571f417
FlavinouTheOne <flavien.dumas63@gmail.com>
2023-09-25 19:11:01 +0200

    dynamic pokemon list added

--- src/components/Pokemoncard.jsx
+++ src/components/Pokemoncard.jsx
@@ -1,10 +1,28 @@
-
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 const PokemonCard = () => {
+    const pokemon = pokemonList[0];
+    const imgHandler = (pokemon) => {
+        if (typeof(pokemon.imgSrc) === "string") {
+            return <img src={pokemon.imgSrc}></img>
+        } else {
+            return <p>???</p>;
+        }
+
+    }
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemonCard" />
-            <figcaption>Bulbasaur</figcaption>
+            {imgHandler(pokemon)}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     )
 }
